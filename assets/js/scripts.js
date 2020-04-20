9460fa92fe379d51035db9a12f3aab3225c778ed
20200425 <61525725+BS-HS@users.noreply.github.com>
2020-04-20 16:07:41 +0900

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -220,7 +220,7 @@ $(document).ready(function(){
         }
     });
     window.onpopstate = history.onpushstate = function(e) {
-        if(window.location.href.split('/').pop().indexOf('modal#myModal')===-1){ // 마지막 segment가 cards라면 모달이 아닌 리스트인 상태이어야한다.
+        if(window.location.href.split('/').pop().indexOf('modal#myModal')===-2){ // 마지막 segment가 cards라면 모달이 아닌 리스트인 상태이어야한다.
             modal.close(); // 현재의 모달을 닫는다.
         }
     }
